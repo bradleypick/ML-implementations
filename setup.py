e31be6c94dfc128f169a580124a15b34897f1f5b
bradleypick <bradleypick@gmail.com>
2017-12-03 08:58:56 -0800

    trying to find a satisfactory directory structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name = 'mymlimp',
+      version='0.1',
+      description='My machine learning implementations',
+      url='http://github.com/bradleypick/mymlimp',
+      author='Bradley Pick',
+      author_email='bradleypick@gmail.com',
+      license='MIT',
+      packages=['mymlimp'],
+      zip_safe=False)
