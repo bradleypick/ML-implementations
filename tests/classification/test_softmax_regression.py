041f5f2e78e11b71d13b70d7a985ae1745e2e55a
bradleypick <bradleypick@gmail.com>
2018-03-23 10:52:47 -0700

    add test cases for loss, grad_loss, and gradient descent

--- tests/classification/test_softmax_regression.py
+++ tests/classification/test_softmax_regression.py
@@ -29,3 +29,36 @@ def test_rowwise_log_sum_exp():
 	"""
 	lse_arr = softmax_regression.log_sum_exp(arr=X)
 	assert X.shape[0] == lse_arr.shape[0]
+
+def test_gradient_descent():
+	"""
+	Test that gradient descent is returning array of correct shape
+	and that it minimizes the norm of gradient on toy example
+	"""
+	f = lambda x: x**2
+	grad_f = lambda x: 2*x
+	w0 = 5.0
+	eps = 0.0001
+	gd = softmax_regression.gradient_descent(f, grad_f, w0, eps=eps)
+	assert type(gd) == type(w0)
+	assert np.isclose(gd, 0, atol=eps)
+
+def test_loss():
+	"""
+	Test that softmax loss returns an objecct of type float
+	"""
+	k = len(np.unique(y))
+	n, d = X.shape
+	W = np.random.random((k,d))
+	loss = softmax_regression.loss(W, X, y)
+	assert isinstance(loss, float)
+
+def test_grad_loss():
+	"""
+	Test that gradient of softmax loss returns array of correct shape
+	"""
+	k = len(np.unique(y))
+	n, d = X.shape
+	W = np.random.random((k,d))
+	grad_loss = softmax_regression.grad_loss(W, X, y)
+	assert grad_loss.shape == W.shape
