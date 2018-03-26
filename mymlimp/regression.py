884b1c493025919915b8ff7f49bcc96af4ca7d87
bradleypick <bradleypick@gmail.com>
2018-03-26 11:17:02 -0700

    add bones of linear model - placeholders so future tests run

---
+++ mymlimp/regression.py
@@ -0,0 +1,31 @@
+#! usr/bin/env python
+
+import numpy as np
+
+class linear_model():
+
+    def __init__():
+
+        self.w = None
+
+    def fit(self, X, y, reg=None):
+        """
+        External method for fitting linear model
+        Inputs:
+         - X:   feature (design) matrix of dimension n x d
+         - y:   reponse variable (array of length n)
+         - reg: type of regularization to use (default is None)
+        Output:
+         - None, called for side effects of updating class attribute w
+        """
+        pass
+
+    def predict(X):
+        """
+        External method for making predictions
+        Inputs:
+         - X: n x d array of observations to make predictions on
+        Output:
+         - an array of length n correponding to predictions for each obs
+        """
+        pass
