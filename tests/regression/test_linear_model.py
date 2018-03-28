2fe51039d0a83815909e4bc20ba8f953b9bf07c2
bradleypick <bradleypick@gmail.com>
2018-03-27 19:32:45 -0700

    small tweak - need to move on to other things so putting here

--- tests/regression/test_linear_model.py
+++ tests/regression/test_linear_model.py
@@ -4,8 +4,12 @@ import numpy as np
 import pytest
 import sys
 sys.path.append('../')) # not os agnostic
+from sklearn.datasets import load_linnerud
 
 from mymlimp.regression import linear_model
+
+X, y = load_linnerud(return_X_y=True)
+
 
 def test_fit():
     """
