490bef6f3fbf8688de9c8a3304e89deb886dd47f
bradleypick <bradleypick@gmail.com>
2018-04-04 22:00:33 -0700

    working linear model and l2 regularization

--- mymlimp/regression.py
+++ mymlimp/regression.py
@@ -4,11 +4,11 @@ import numpy as np
 
 class linear_model():
 
-    def __init__():
+    def __init__(self):
 
         self.w = None
 
-    def fit(self, X, y, reg=None):
+    def fit(self, X, y, reg=None, lam=0.001):
         """
         External method for fitting linear model
         Inputs:
@@ -18,9 +18,13 @@ class linear_model():
         Output:
          - None, called for side effects of updating class attribute w
         """
-        pass
+        n, d = X.shape
+        if reg is None:
+            self.w = np.linalg.solve(X.T @ X, X.T @ y)
+        elif reg == 'l2':
+            self.w = np.linalg.solve((X.T @ X + lam*np.eye(n)), X.T @ y)
 
-    def predict(X):
+    def predict(self, X):
         """
         External method for making predictions
         Inputs:
@@ -28,4 +32,4 @@ class linear_model():
         Output:
          - an array of length n correponding to predictions for each obs
         """
-        pass
+        return X @ self.w
